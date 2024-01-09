953c9ac014305c5ddefc8444cd9eb0bc81f8ef4a
Mikhail8425 <mikhailshemet@gmail.com>
2024-01-09 13:59:53 -0500

    head.js working

--- lotide/head.js
+++ lotide/head.js
@@ -1,8 +1,12 @@
+const head = function (arg) {
+  console.log(arg[0]);
+}
 
 const assertEqual = function(actual, expected) {
 
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
