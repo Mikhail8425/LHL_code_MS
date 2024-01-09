d0cc0b86d77e5c7f3559d79d966d966261f090ae
Mikhail8425 <mikhailshemet@gmail.com>
2024-01-09 11:09:55 -0500

    assertEqual

---
+++ lotide/assertEqual.js
@@ -0,0 +1,16 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/console/assert_static
+console.assert(1 === 1); // => nothing happens because true
+console.assert(1 === 1.1); // => prints out "Assertion failed" to the terminal
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual == expected) {
+      console.log(true)
+  } else {
+      console.log(false)
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
