184e33ae77015e0360e4471c78c60d3d01f0dc30
Mikhail8425 <mikhailshemet@gmail.com>
2024-01-16 10:56:50 -0500

    move lotide out of it

--- lotide/test/assertEqualTest.js
+++
@@ -1,8 +0,0 @@
-const assertEqual = require('../assertEqual');
-
-const assertEqual = function(actual, expected) {
-  if (actual == expected) {
-    return(actual == expected)
-  } 
-};
-module.exports = assertEqual;
